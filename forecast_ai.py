3dcf3120a04ee49b62b48bea09a6740747915d30
raunakapp1 <bikerjacket16@gmail.com>
2026-03-01 14:23:10 +0530

    Update forecast_ai.py

--- forecast_ai.py
+++ forecast_ai.py
@@ -1,4 +1,13 @@
-import random
+def predict_tomorrow_revenue(today_revenue):
+    """
+    Simple AI logic:
+    +12% Growth Model
+    """
 
-def predict_tomorrow_revenue(today):
-    return int(today * random.uniform(1.1,1.3))
+    try:
+        today_revenue = float(today_revenue)
+    except:
+        return 0
+
+    growth_factor = 1.12
+    return today_revenue * growth_factor
