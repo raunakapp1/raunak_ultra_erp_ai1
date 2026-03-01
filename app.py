7851feb507a159fd0616a85c0f6b5574e5cb3a31
raunakapp1 <bikerjacket16@gmail.com>
2026-03-01 14:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
 import streamlit as st
-import sys
-import os
-
-# 🔥 ROOT PATH FIX
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, BASE_DIR)
-
-from dashboard import admin_dashboard
-
-def main():
-    admin_dashboard()
-
-if __name__ == "__main__":
-    main()
-    import streamlit as st
 from dashboard import admin_dashboard
 
 admin_dashboard()
-
