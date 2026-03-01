893483060622e0730f5f7eaf20386cb1a10e2420
raunakapp1 <bikerjacket16@gmail.com>
2026-03-01 14:38:26 +0530

    Update forecast_ai.py

--- forecast_ai.py
+++ forecast_ai.py
@@ -1,6 +1,5 @@
 def predict_tomorrow_revenue(today_revenue):
     try:
-        today_revenue = float(today_revenue)
-        return today_revenue * 1.12
+        return float(today_revenue) * 1.12
     except:
         return 0
