60cbcf3e34b6c3fd05ad1134220ab78b996e3427
raunakapp1 <bikerjacket16@gmail.com>
2026-03-02 14:49:08 +0530

    Update login.py

--- login.py
+++ login.py
@@ -3,20 +3,13 @@ import streamlit as st
 def login_page():
     st.title("🔐 Login")
 
-    users = {
-        "admin": {"password": "admin123", "role": "admin"},
-        "staff1": {"password": "1234", "role": "staff"},
-        "staff2": {"password": "1234", "role": "staff"}
-    }
-
-    username = st.selectbox("Select User", list(users.keys()))
-    password = st.text_input("Password", type="password")
+    user = st.text_input("Username")
+    pwd = st.text_input("Password", type="password")
 
     if st.button("Login"):
-        if users[username]["password"] == password:
+        if user == "admin" and pwd == "1234":
             st.session_state.login = True
-            st.session_state.role = users[username]["role"]
             st.success("Login Successful")
             st.experimental_rerun()
         else:
-            st.error("Wrong Password")
+            st.error("Invalid Login")
